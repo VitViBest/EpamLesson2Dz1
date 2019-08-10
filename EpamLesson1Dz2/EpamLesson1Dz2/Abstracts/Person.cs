173ac9ef054ee28e962cd4232e370f01882f1fbc
Vit_vi <vitalik99968@gmail.com>
2019-08-10 19:29:56 +0300

    Create

---
+++ EpamLesson1Dz2/EpamLesson1Dz2/Abstracts/Person.cs
@@ -0,0 +1,30 @@
+﻿
+using EpamLesson1Dz2.Interfaces;
+
+namespace EpamLesson1Dz2.Abstracts
+{
+    abstract class Person : IPerson
+    {
+        public string Name { get; private set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        // Person go to home.
+        public abstract string Move();
+
+        // Person stoped hear a home.
+        public abstract string Stop();
+
+        // Person enter in home.
+        public abstract string Enter();
+
+        // Person can crash a home.
+        public virtual bool Crashed()
+        {
+            return false;
+        }
+    }
+}
---
+++ EpamLesson1Dz2/EpamLesson1Dz2/Models/Bear.cs
@@ -0,0 +1,44 @@
+﻿using EpamLesson1Dz2.Abstracts;
+using System;
+
+namespace EpamLesson1Dz2.Models
+{
+    class Bear : Person
+    {
+        private int _Mass;
+
+        private int _Height;
+
+        private Random _Random;
+
+        public Bear(string name, int mass, int height) : base("Медведь-" + name)
+        {
+            _Mass = mass;
+            _Height = height;
+            _Random = new Random();
+        }
+
+        public override bool Crashed()
+        {
+            return _Random.Next(_Mass * _Height) > _Mass * _Height / 2;
+        }
+
+        public override string Enter()
+        {
+            string[] actions = { "Залез", "Вошел", "Забрался" };
+            return actions[_Random.Next(actions.Length)];
+        }
+
+        public override string Move()
+        {
+            string[] actions = { "Пробирается", "Идет" };
+            return actions[_Random.Next(actions.Length)];
+        }
+
+        public override string Stop()
+        {
+            string[] actions = { "Посмотрел", "Подошел" };
+            return actions[_Random.Next(actions.Length)];
+        }
+    }
+}
